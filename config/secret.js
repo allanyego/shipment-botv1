b146a015ea01cbeaa8892e0c2e5cfdcf52708738
yegow <33445976+yegow@users.noreply.github.com>
2019-01-07 18:31:38 +0300

    Delete secret.js
    This is meant to be a private file.

--- config/secret.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  messengerAccessToken: "EAAHz5f6oW1cBAHXwsBAOYLWUTmfqRPkXJs4VcTZCo9ZBwkMkeVxBEHvopT" +
-  "914SIvIfYqfzwPviGuO9rEQfyrJHqOAFhVFyBfwvQ5wLhF7QXPLWp1bRWpNQbbqj8PZAvNrJjA" +
-  "ybsIReWY1qMG3m2ah7Jx9Dhx2arW22uiWbZCBVdZBZCJ7OQXZABilPIVKf2y5MZD",
-  postnordApiKey: "45e16936c798ec9d7d8ca0ad66de9688"
-};
